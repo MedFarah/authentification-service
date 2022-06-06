f89770420253861245a0ab376e7354d144629632
Mohamed Farah <hamouchka7@gmail.com>
2022-06-06 13:13:08 +0100

    fix cors problem

--- src/main/java/org/tn/zitouna/configuration/SecurityConfig.java
+++ src/main/java/org/tn/zitouna/configuration/SecurityConfig.java
@@ -1,8 +1,11 @@
 package org.tn.zitouna.configuration;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -13,6 +16,9 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.tn.zitouna.service.UserService;
 
 @Configuration
@@ -44,13 +50,23 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-				http.csrf().disable().cors().and()
+				http.csrf().disable().cors().configurationSource(corsConfigurationSource()).and()
 		.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-		.and().authorizeRequests().antMatchers("/login","/register","/swagger-ui/index.html").permitAll()
+		.and().authorizeRequests().antMatchers("/login","/register").permitAll()
 		.anyRequest().authenticated().and().addFilterBefore(this.jwtTokenAuthorizationFilter, UsernamePasswordAuthenticationFilter.class);
 		
 	}
 	
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+	    CorsConfiguration configuration = new CorsConfiguration();
+	    configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+	    configuration.setAllowedMethods(Arrays.asList("GET","POST","PUT","DELETE"));
+	    configuration.setAllowedHeaders(Arrays.asList("*"));
+	    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+	    source.registerCorsConfiguration("/**", configuration);
+	    return source;
+	}
 	@Override
 	@Bean
 	public AuthenticationManager authenticationManagerBean() throws Exception {
--- src/main/java/org/tn/zitouna/web/UserController.java
+++ src/main/java/org/tn/zitouna/web/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,6 +18,7 @@ import org.tn.zitouna.entities.AppUser;
 import org.tn.zitouna.service.UserService;
 
 @RestController
+//@CrossOrigin(origins = "http://localhost:4200")
 public class UserController {
 
 	private UserService userService;
@@ -42,7 +44,6 @@ public class UserController {
 
 	@PostMapping(path = "/login")
 	public ResponseEntity<AppUser> login(@RequestBody AppUser userr) {
-		System.out.println("hani1");
 		this.authenticate(userr.getUsername(), userr.getPassword());
 
 		AppUser user = userService.findUserByUserName(userr.getUsername());
